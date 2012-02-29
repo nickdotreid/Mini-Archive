5c458c0acbf83c9fc54d2d499b1f24ba643101e2
Nick Reid <nickreid@nickreid.com>
2012-02-28 16:44:20 -0800

    made container around get_template part

--- draw.php
+++ draw.php
@@ -7,8 +7,14 @@ function mini_archive_draw(){
 	if(is_page()){
 		$archive_value = get_post_meta(get_the_ID(),'mini_archive',true);
 		if($archive_value){
+			?>
+			<aside class="mini_archive">
+			<?
 			$wp_query = new WP_Query( 'post_type='.$archive_value );
 			get_template_part( 'loop', $archive_value );
+			?>
+			</aside>
+			<?
 		}
 	}
 	$wp_query = $old_query;
