a5a3753eda5c4f328793180f917b6d975c5b081f
Nick Reid <nickreid@nickreid.com>
2012-02-29 16:40:14 -0800

    seperated functions from draw to seperate file for resuability

--- draw.php
+++ draw.php
@@ -1,41 +1,22 @@
 <?php
 
+include_once('functions.php');
+
 add_action('get_footer','mini_archive_draw',10);
 function mini_archive_draw(){
-	if(is_page()){
-		$archive_value = get_post_meta(get_the_ID(),'mini_archive',true);
-		if($archive_value){
+	if(is_page() && mini_archive_on_page()){
+			$archive_type = get_post_meta(get_the_ID(),'mini_archive',true);
+			$query = mini_archive_get_query();
 			?>
 			<aside class="mini_archive">
 			<?
-			$tax_query = array('relation'=>'AND');
-			$filters = get_post_meta(get_the_ID(),'mini_archive_filters',false);
-			foreach($filters as $filter){
-				$filter = unserialize($filter);
-				$query = array(
-					'taxonomy' => $filter['type'],
-					'field' => 'slug',
-					'terms' => $filter['term'],
-				);
-				if(array_key_exists('operator',$filter)){
-					$query['operator'] = $filter['operator'];
-				}
-				array_push($tax_query,$query);
-			}
-			$query = new WP_Query(array(
-				"post_type"=>$archive_value,
-				"post_status"=>'publish',
-				"order_by" => 'menu_order date title',
-				"tax_query" => $tax_query
-			));
 			while($query->have_posts()) {
 				$query->the_post();
-				get_template_part( 'content', $archive_value );
+				get_template_part( 'content', $archive_type );
 			}
 			?>
 			</aside><!-- .mini-archive -->
 			<?
-		}
 	}
 }
 
---
+++ functions.php
@@ -0,0 +1,48 @@
+<?php
+
+function mini_archive_on_page($ID=false){
+	if(!$ID){
+		$ID = get_the_ID();
+	}
+	$archive = get_post_meta($ID,'mini_archive',true);
+	if($archive){
+		return $ID;
+	}
+	return false;
+}
+
+function mini_archive_get_query($ID=false){
+	$ID = mini_archive_on_page($ID);
+	if(!$ID){
+		return false;
+	}
+	
+	$query = new WP_Query(array(
+		"post_type"=>get_post_meta($ID,'mini_archive',true),
+		"post_status"=>'publish',
+		"order_by" => 'menu_order date title',
+		"tax_query" => mini_archive_get_tax_query($ID),
+	));
+	return $query;
+}
+
+function mini_archive_get_tax_query($ID){
+	$tax_query = array('relation'=>'AND');
+	$filters = get_post_meta($ID,'mini_archive_filters',false);
+	foreach($filters as $filter){
+		$filter = unserialize($filter);
+		$query = array(
+			'taxonomy' => $filter['type'],
+			'field' => 'slug',
+			'terms' => $filter['term'],
+		);
+		if(array_key_exists('operator',$filter)){
+			$query['operator'] = $filter['operator'];
+		}
+		array_push($tax_query,$query);
+	}
+	
+	return $tax_query;
+}
+
+?>
