851d09f54bd7e53a0a33b6b254cf3d03f2dbad54
Nick Reid <nickreid@nickreid.com>
2012-03-05 13:03:05 -0800

    added archive type to aside element

--- draw.php
+++ draw.php
@@ -5,10 +5,10 @@ include_once('functions.php');
 add_action('get_footer','mini_archive_draw',10);
 function mini_archive_draw(){
 	if(is_page() && mini_archive_on_page()){
-			?>
-			<aside class="mini_archive">
-			<?
 			$archive_type = get_post_meta(get_the_ID(),'mini_archive',true);
+			?>
+			<aside class="mini_archive <?=$archive_type;?>">
+			<?
 			$query = mini_archive_get_query();
 			do_action('mini_archive_before');
 			while($query->have_posts()) {
